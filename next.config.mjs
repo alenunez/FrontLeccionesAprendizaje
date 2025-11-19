82f58bc98b4709f2cb0d66a510498b085e6e59d4
Alejandro Nuñez Herrera <alenunez097@gmail.com>
2025-11-18 22:38:33 -0500

    feat: expand dashboard layout

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.solla.com",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
   },
 }
 

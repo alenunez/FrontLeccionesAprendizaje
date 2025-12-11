2f14f9df7cffa8a31e001818a16b32134df5c224
Alejandro Nuñez Herrera <alenunez097@gmail.com>
2025-12-11 10:29:09 -0500

    Ensure static export for project pages

--- app/proyectos/editar/page.tsx
+++ app/proyectos/editar/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react"
 
 import { ProjectEditorPageClient } from "./client-page"
 
+export const dynamic = "force-static"
+
 
 export default function ProjectEditorPage() {
   return (
